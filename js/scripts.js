52a4c0f026dbd9b4eb0a0372758eb03ccd552862
kolmansoifer <kolmansoifer@gmail.com>
2020-08-14 12:17:14 -0700

    Create radio list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
-  $("form#language").submit(function() {
+  $("form#language").submit(function(event) {
+    const goals = $("input#goals").val();
+    const skills = $("input#skills").val();
+    const interests = $("input#interests").val();
+    const platforms = $("input#platforms").val();
+
     event.preventDefault();
-    const 
