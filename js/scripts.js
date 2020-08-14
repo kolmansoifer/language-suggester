183a46566b64ff5268f6c2130b7bc62f96f6d93b
kolmansoifer <kolmansoifer@gmail.com>
2020-08-14 14:19:46 -0700

    Fix bugs found in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $("form#language").submit(function(event) {
     const goals = $("input#goals").val();
+    const salary = $("input#salary").val();
     const skills = $("input#skills").val();
     const interests = $("input#interests").val();
     const platforms = $("input#platforms").val();
